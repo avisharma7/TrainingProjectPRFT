a33d1608a55a3295c81ed61a14f0991f0a0fb178
S Neeraj <sneeraj2001@gmail.com>
2024-07-08 15:03:14 +0530

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -1,6 +1,7 @@
 def user():
+    print("\tUSER LOGIN")
     comp_name = input("Enter the name of Company : ")
-    symbol = input("Enter the symbol of the company : ")
+    symbol = (input("Enter the symbol of the company : ")).upper()
     print("Select an option")
     print("1. Daily")
     print("2. Weekly")
@@ -10,24 +11,27 @@ def user():
     if time == 1:
         daily_data(comp_name, symbol)
     
-    if time == 2:
+    elif time == 2:
         weekly_data(comp_name, symbol)
 
-    if time == 3:
+    elif time == 3:
         monthly_data(comp_name, symbol)
     
     else:
-        int(input("Invalid option. Please enter again "))
-    
+        int(input("Invalid option. Please enter again : "))
+        
+
+
 
 def company():
-    comp_name = input("Enter the name of your company.")
-    symbol = input("Enter the symbol of your company.")
+    print("\tCOMPANY LOGIN")
+    comp_name = input("Enter the name of your company : ")
+    symbol = input("Enter the symbol of your company : ")
     open_price = int(input("Enter the opening price of your stock : "))
     close_price = int(input("Enter the closing price of your stock : "))
     low_price = int(input("Enter the lowest price of your stock : "))
     high_price = int(input("Enter the highest price of your stock : "))
-    vol = int(input("Enter the volume of the stock traded"))
+    vol = int(input("Enter the volume of the stock traded :  "))
 
 
 def daily_data(company_name, symbol):
@@ -42,9 +46,9 @@ def monthly_data(company, symbol):
 def main_menu():
     while True:
         print("\nMain Menu")
-        print("\nUSER")
-        print("\nCOMPANY")
-        print("\nEXIT")
+        print("USER")
+        print("COMPANY")
+        print("EXIT")
         type = (input("Select your login mode : ")).upper()
 
 
